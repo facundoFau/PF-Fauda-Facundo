b98da86d4e79307dee1f549047d9588c20c99e3e
Facundo Fauda <facundo.fauda2000@gmail.com>
2022-12-13 12:04:55 -0300

    agregando js

---
+++ main.js
@@ -0,0 +1,8 @@
+const main = document.getElementById('main');
+const agregarUsuario = document.getElementById('agregar-usuario');
+const duplicarDinero = document.getElementById('doblar');
+const mostrarMillonarios = document.getElementById('mostrar-millonarios');
+const ordenar = document.getElementById('ordenar');
+const calcularTotal = document.getElementById('calcular-total');
+
+
